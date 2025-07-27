47fc10c3c368134b8aeef7247c5554c808381dff
hannahscovill <hnsvill@gmail.com>
2025-07-27 16:29:13 -0700

    footer and favicon

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,56 @@
 import Collage from "./components/TsfSign/Collage"
 import MenuButton from "./components/MenuButton"
+import Footer from "./components/Footer"
 
+const SectionHeader = ({ copy, className }: { copy: string, className?: string }) => {
+  return (
+    <div className={`${className} my-4 mx-[1rem] md:my-0 md:ml-4 md:row-start-2 md:col-start-2 order-0 flex flex-col justify-end`}>
+      <h2 className="text-4xl font-bold">
+        {copy}
+      </h2>
+      <hr className="border-t-4 w-[70%] my-2" />
+    </div>
+  )
+}
 function App() {
 
   return (
-    <div>
-      <div className="grid mx-auto max-w-[1600px] grid-cols-1 md:grid-cols-2 md:grid-rows-4 m-8">
-        <div className="my-4 md:my-0 md:ml-4 md:row-start-2 md:col-start-2 order-0 flex flex-col justify-end">
-          <h2 className="text-4xl font-bold">
-            Hannah Scovill
-          </h2>
-          <hr className="border-t-4 w-[70%] my-2" />
+    <>
+      <div>
+        <div className="grid mx-auto max-w-[1600px] grid-cols-1 md:grid-cols-2 md:grid-rows-4 m-8">
+          <SectionHeader copy="Hannah Scovill" />
+          <p className="my-6 md:my-0 order-3 md:ml-4 md:row-start-3 md:col-start-2 m-2 flex justify-center md:items-start">Software Engineer, CrossFit Coach, Student Pilot, always curious.</p>
+          <Collage className="order-1 col-start-1 row-span-4" />
         </div>
-        <p className="my-6 md:my-0 order-3 md:ml-4 md:row-start-3 md:col-start-2 flex items-start">Software Engineer, CrossFit Coach, Student Pilot, always curious.</p>
-        <Collage className="order-1 col-start-1 row-span-4" />
-      </div>
 
-      <div className="flex justify-center mt-8">
-        <MenuButton buttonCopy="Download Resume" onClick={() => window.open('/resume.pdf', '_blank')} className="w-64 h-16" />
+        <div className="flex justify-center mt-8">
+          <MenuButton buttonCopy="Download Resume" onClick={() => window.open('/resume.pdf', '_blank')} className="w-64 h-16" />
+          <a href='mailto:hnsvill@gmail.com?subject=Re:%20@%20you' target="_blank">
+          <MenuButton buttonCopy="@ me" className="w-64 h-16" />
+          </a>
+        </div>
+
+        <div className="max-w-[1600px] flex flex-col justify-self-center">
+          <SectionHeader className="pt-10" copy="Short & Sweet" />
+          <p className="mx-[1rem]">
+            People describe me as proactive and resourceful. I learned to code in my administrative assistant job in 2016 and U.S. Bank's Mortgage division used my apps.
+            <br /><br />
+            My tech experience has been mostly backend, data and DevOps oriented. I'm currently gaining professional experience in React, serving on Nike's PRIDE Network board, coaching CrossFit and learning to fly.
+            <br /><br />
+            I'm based in the Portland, Oregon area.
+          </p>
+        </div>
+
+        <div className="max-w-[1600px] flex flex-col justify-self-center">
+          <SectionHeader className="pt-10" copy="Tech Stack" />
+          <p className="mx-[1rem] w-full">
+            test
+          </p>
+        </div>
+
       </div>
-    </div>
+      <Footer />
+    </>
   )
 }
 
--- src/components/MenuButton.tsx
+++ src/components/MenuButton.tsx
@@ -1,12 +1,13 @@
-interface MenuButtonProps {
-  buttonCopy: string
-  onClick?: () => void
-  className?: string
-}
-
-const MenuButton = ({ buttonCopy, onClick, className }: MenuButtonProps) => {
+const MenuButton = ({
+  buttonCopy,
+  onClick,
+  className }: {
+    buttonCopy: string
+    onClick?: () => void
+    className?: string
+  }) => {
   return (
-    <div className={`relative m-[1rem] ${className}`}>
+    <div className={`relative m-[1rem] min-w-[120px] ${className}`}>
       <div
         className="sage-box absolute w-full h-full -z-10 translate-x-[.5rem] translate-y-[.5rem]"
         style={{ backgroundColor: '#949c8f' }}
@@ -14,14 +15,14 @@ const MenuButton = ({ buttonCopy, onClick, className }: MenuButtonProps) => {
       <div className="group ">
 
         <div
-          className="white-box bg-white absolute w-full h-full z-19 translate-x-[-.5rem] translate-y-[-.5rem] group-hover:translate-x-[-1rem] group-hover:translate-y-[-1rem]"
+          className="white-box bg-white absolute w-full h-full z-19 translate-x-[-.5rem] translate-y-[-.5rem] group-hover:translate-x-[-.75rem] group-hover:translate-y-[-.75rem] group-active:translate-x-[-.25rem] group-active:translate-y-[-.25rem] transition-all duration-200"
         />
         <div
-          className="absolute border-5 w-full h-full z-20 flex items-center justify-center font-bold group-hover:translate-x-[-.5rem] group-hover:translate-y-[-.5rem] "
+          className=" absolute border-5 w-full h-full z-20 flex items-center justify-center font-bold group-hover:translate-x-[-.25rem] group-hover:translate-y-[-.25rem] group-active:translate-x-[.25rem] group-active:translate-y-[.25rem] transition-all duration-200"
         >
           <button
             onClick={onClick}
-            className="relative font-bold text-lg"
+            className="relative font-bold text-lg m-2"
           >
             {buttonCopy}
           </button>
