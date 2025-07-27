327a1f3152775550acf8a05f73176d2b69712a89
hannahscovill <hnsvill@gmail.com>
2025-07-27 14:42:31 -0700

    button hover translate

--- src/components/MenuButton.tsx
+++ src/components/MenuButton.tsx
@@ -11,18 +11,21 @@ const MenuButton = ({ buttonCopy, onClick, className }: MenuButtonProps) => {
         className="sage-box absolute w-full h-full -z-10 translate-x-[.5rem] translate-y-[.5rem]"
         style={{ backgroundColor: '#949c8f' }}
       />
-      <div
-        className="white-box bg-white absolute w-full h-full z-19 translate-x-[-.5rem] translate-y-[-.5rem]"
-      />
-      <div
-        className="absolute border-5 w-full h-full z-20 flex items-center justify-center font-bold"
-      >
-        <button
-          onClick={onClick}
-          className="relative font-bold text-lg"
+      <div className="group ">
+
+        <div
+          className="white-box bg-white absolute w-full h-full z-19 translate-x-[-.5rem] translate-y-[-.5rem] group-hover:translate-x-[-1rem] group-hover:translate-y-[-1rem]"
+        />
+        <div
+          className="absolute border-5 w-full h-full z-20 flex items-center justify-center font-bold group-hover:translate-x-[-.5rem] group-hover:translate-y-[-.5rem] "
         >
-          {buttonCopy}
-        </button>
+          <button
+            onClick={onClick}
+            className="relative font-bold text-lg"
+          >
+            {buttonCopy}
+          </button>
+        </div>
       </div>
     </div>
   )
